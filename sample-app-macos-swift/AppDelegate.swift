92673fc797e7a3a2e8ac9130221c5edcd34f3f14
Ivan Matkov <v-ivmatk@microsoft.com>
2017-09-06 12:09:31 +0300

    Add blank project

---
+++ sample-app-macos-swift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  sample-app-macos-swift
+//
+//  Created by Akvelon on 9/6/17.
+//  Copyright © 2017 Microsoft. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ sample-app-macos-swift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  sample-app-macos-swift
+//
+//  Created by Akvelon on 9/6/17.
+//  Copyright © 2017 Microsoft. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
