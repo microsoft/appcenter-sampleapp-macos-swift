9db65ae9a3d33fbc4c261a0b6e45d71d4b126a81
Ivan Matkov <v-ivmatk@microsoft.com>
2017-10-04 15:05:49 +0300

    Mobile Center initialisation moved to applicationDidFinishLaunching

--- sample-app-macos-swift/AppDelegate.swift
+++ sample-app-macos-swift/AppDelegate.swift
@@ -7,15 +7,24 @@ import MobileCenterPush
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
   
-  override init() {
-    super.init()
-    
+  var rootController: NSWindowController!
+  
+  func applicationDidFinishLaunching(_ notification: Notification) {
+
     // Start mobile center
     MSMobileCenter.start("<APP SECRET HERE>", withServices: [
       MSAnalytics.self,
       MSCrashes.self,
       MSPush.self,
     ])
+    
+    initUI()
+  }
+  
+  func initUI() {
+    let mainStoryboard = NSStoryboard.init(name: "Main", bundle: nil)
+    rootController = mainStoryboard.instantiateController(withIdentifier: "rootController") as! NSWindowController
+    rootController.showWindow(self)
+    rootController.window?.makeKeyAndOrderFront(self)
   }
 }
-
